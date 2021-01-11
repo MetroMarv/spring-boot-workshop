c5de51572936ad511cda83a700517bd984dc6701
Marvin Rensing <marvin.rensing@gea.com>
2021-01-11 16:52:05 +0100

    Minimal Spring Boot + Docker Example

---
+++ src/main/java/com/gea/springbootworkshop/SpringBootWorkshopApplication.java
@@ -0,0 +1,20 @@
+package com.gea.springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWorkshopApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello GEA World";
+    }
+}
